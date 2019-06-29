d561658347ab46ce435846c1388942d2225d2d5f
Marco Angel <angel2367716@gmail.com>
2019-06-29 13:26:50 -0700

    Started initial notes for when user presses enter key

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,24 @@
-//test
+//pull information from form and build the query URL
+
+
+//Click handlers
+//using the on click function event, it allows the user to hit enter on the keyboard and it registers the search 
+$("#runSearch-we-need-to-define-this-id-first").on("click", function (event){
+//#runSearch is a potential id name 
+    //prevents page from reloading on form submit
+    event.preventDefault();
+
+    //empties the region associated with the article 
+    clear();
+
+
+    //Building the query URL for the ajax request to the bls API
+    
+        //const queryURL = buildQueryURL();
+
+    //Makes Ajax request to the API 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(updatePage);
+});
