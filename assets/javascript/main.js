3a7c59d43a7a46b87f1adee636b583a479f5dce8
Marco Angel <angel2367716@gmail.com>
2019-07-02 19:36:32 -0700

    Added a way to possibly remove the titlepage after searching for something

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,37 +1,35 @@
+$(document).ready(function () {
 //pull information from form and build the query URL
 
 
 //Click handlers
 //using the on click function event, it allows the user to hit enter on the keyboard and it registers the search 
-$("#occupation").on("click", function (event){
+$("#submitOccupation").on("click", function (event) {
 
-// Grab value from User input field
-const OcupationInput = $('input').val();
-const blsAPIKey
-// URL for ajex query
-const queryURL = "https:api.bls.gov/publicAPI/v2/timeseries/data/?registrationkey=" +
-blsAPIKey + "&seriesid=series1";
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    const results = response;
-    console.log(response);
-})
-    //prevents page from reloading on form submit
-    event.preventDefault();
-
-    //empties the region associated with the article 
-    clear();
-
-
-    //Building the query URL for the ajax request to the bls API
-    
-        //const queryURL = buildQueryURL();
-
-    //Makes Ajax request to the API 
+    // Grab value from User input field
+    const OcupationInput = $('input').val();
+    const blsAPIKey
+    // URL for ajex query
+    const queryURL = "https:api.bls.gov/publicAPI/v2/timeseries/data/?registrationkey=" +
+        blsAPIKey + "&seriesid=series1";
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(updatePage);
+    }).then(function (response) {
+        const results = response;
+        console.log(response);
+    });
+    //prevents page from reloading on form submit
+    event.preventDefault();
+    //empties the region associated with the article 
+    clear();
+    //hides tittle
+    $("#titlePage").hide();//I'm not entirely sure this will be enough to change the tittle page
+
+    //This section should generate a new page result, but i need to figure out how to and where to put the results
+    $( "#result" ).load( "", function() {
+        alert( "Load was performed." );
+      });
+      //This other section should be for generating a new div that can be used to create a table 
+      });
 });
