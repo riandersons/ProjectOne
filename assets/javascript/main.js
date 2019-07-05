7e6866fa3b936088071492c77da7625d65ffcc86
unknown <Mendez.AndrewM@gmail.com>
2019-07-05 11:33:09 -0700

    cleaned up the ajax query

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,24 +8,22 @@ $('document').ready(function () {
         event.preventDefault();
 
         //pull information from form and build the query URL
-        const OcupationInput = $('input').val();
+        const OcupationInput = $('input').val().trim();
         console.log('Occupation entered:  ' + OcupationInput);
 
         $('#city').html(city);
 
         const blsAPIKey = "44ac302872a44634a6809cf464899d3e"
-        // URL for ajex query
-        const queryURL = "https:api.bls.gov/publicAPI/v2/timeseries/data/?registrationkey=" +
-            blsAPIKey + "&seriesid=series1";
+        const queryURL = "https:api.bls.gov/publicAPI/v2/timeseries/data/search?api_key=" +
+            blsAPIKey + "/seriesid=OEUN000000011100011000001&areatype_code=M&occupation_name=" + OcupationInput;
 
         $.ajax({
-            // url: queryURL + "&seriesid=OEUN000000056",
-            url: "https://api.bls.gov/publicAPI/v2/timeseries/data/?registrationkey=44ac302872a44634a6809cf464899d3e&catalog=true&startyear=2010&endyear=2014&calculations=true&annualaverage=true",
-            method: "POST"
-            
+            dataType: "JSON",
+            url: queryURL,
+            method: "GET",
+
         }).then(function (response) {
-            const results = JSON.parse(response);
-            console.log(results);
+            console.log(response);
 
             // empty variables that will have data assigned to them
             const city = "Anytown";
@@ -34,7 +32,7 @@ $('document').ready(function () {
             const demand = '3,000';
             const growthProjection = '4';
 
-            
+
             // Append data to new table row
             let newRow = $("<tr>").append(
                 $("<td>").text(city),
@@ -42,20 +40,20 @@ $('document').ready(function () {
                 $("<td>").text(avgPay),
                 $("<td>").text(demand),
                 $("<td>").text(growthProjection),
-                );
-                
-                // Append the new row to the table
-                $("#results-table > tbody").append(newRow);
-                
-                console.log(city);
-                console.log(medianPay);
-                console.log(avgPay);
-                console.log(demand);
-                console.log(growthProjection);
-                
-                //empties the region associated with the article 
-                // clear();
-            });
+            );
+
+            // Append the new row to the table
+            $("#results-table > tbody").append(newRow);
+
+            console.log(city);
+            console.log(medianPay);
+            console.log(avgPay);
+            console.log(demand);
+            console.log(growthProjection);
+
+            //empties the region associated with the article 
+            $("#submitOccupation").val().clear();
+        });
     });
 });
 
